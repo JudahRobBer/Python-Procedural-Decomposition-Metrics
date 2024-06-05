e7e14828545a9ba3649e2ffcb6358df77f82c0f7
Alec Wydra <alec.wydra2@gmail.com>
2024-06-05 15:57:49 -0400

    Update communicativeFunctionalityChecker.py

--- communicativeFunctionalityChecker.py
+++ communicativeFunctionalityChecker.py
@@ -11,7 +11,7 @@ class CommunicativeFunctionalityChecker(ast.NodeVisitor):
     checkUniques(node.id)
     self.generic_visit(node)
 
-  def checkUniques(nme: str) :
+  def checkUniques(nme:str):
     if (not self.unique_vals.__contains__(nme)) :
       self.unique_vals.append(nme)
   
