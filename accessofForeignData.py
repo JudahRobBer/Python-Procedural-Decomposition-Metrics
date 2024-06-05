b7484f5b1524502dbe2911f6243f53f7e4fe4bc0
Alec Wydra <alec.wydra2@gmail.com>
2024-06-05 17:09:27 -0400

    Update accessofForeignData.py

--- accessofForeignData.py
+++ accessofForeignData.py
@@ -1,1 +1,10 @@
+import ast
 
+class accessofForeignData(ast.NodeVisitor):
+  def__init__(self):
+    self.assigned = []
+    self.foreign_access_count = 0
+    
+  def visit_Assign(self, node):
+    if (not self.assigned.__contains__(node.targets[0].id)):
+      
