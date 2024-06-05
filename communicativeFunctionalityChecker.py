6974d2f317122d1efbdf2547395cd9bad2ec9f3e
Alec Wydra <alec.wydra2@gmail.com>
2024-06-05 15:33:34 -0400

    Update communicativeFunctionalityChecker.py

--- communicativeFunctionalityChecker.py
+++ communicativeFunctionalityChecker.py
@@ -1,1 +1,21 @@
+import ast
 
+class CommunicativeFunctionalityChecker(ast.NodeVisitor):
+  def __init__(self):
+    self.vars_accessed = []
+
+  def visit_Name(self, node):
+    self.vars_accessed.append(node.id)
+    
+    self.generic_visit(node)
+
+  def getCommunicativeFunctionality(self, package:str, filename: str):
+    with open(f"{package}/{filename}") as file:
+      source_code = file.read()
+       
+      tree = ast.parse(source_code)
+      checker = CommunicativeFunctionalityChecker()
+      checker.visit(tree)
+
+      return len(checker.vars_accessed)
+    
