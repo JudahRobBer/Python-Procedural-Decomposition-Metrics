10c93482b6009fe601795585ec7add8097a2b032
Alec Wydra <alec.wydra2@gmail.com>
2024-06-06 09:08:28 -0400

    Update accessofForeignData.py

--- accessofForeignData.py
+++ accessofForeignData.py
@@ -8,3 +8,20 @@ class accessofForeignData(ast.NodeVisitor):
   def visit_Assign(self, node):
     if (not self.assigned.__contains__(node.targets[0].id)):
       self.assigned.append(node.targets[0].id)
+    generic_visit(node)
+
+  def visit_Name(self, node):
+    if (not self.assigned.__contains__(node.targets[0].id)):
+      self.foreign_access_count += 1
+    generic_visit(node)
+
+def get_Foreign_Access(self, package: str, filename: str):
+  with open(f"{package}/{filename}") as file:
+    source_code = file.read()
+       
+    tree = ast.parse(source_code)
+    checker = accessofForeignData()
+
+    checker.visit(tree)
+
+    return (checker.foreign_access_count)
