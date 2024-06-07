367fff077885bef9d80671b9f48d2d74ee044ebf
Alec Wydra <alec.wydra2@gmail.com>
2024-06-07 15:11:56 -0400

    Update godMethodDetection.py

--- godMethodDetection.py
+++ godMethodDetection.py
@@ -2,4 +2,9 @@ import accessofForeignData, communicativeFunctionalityChecker, complexityMetricG
 
 class godMethodDetector():
   def __init__(self, CFC: dict, AFD: dict, Cyc: dict):
-    
+    self.optimalCFC = CFC
+    self.optimalAFD = AFD
+    self.optimalCyc = Cyc
+
+  def ranked(self):
+    print(sort(self.optimalCFC))
