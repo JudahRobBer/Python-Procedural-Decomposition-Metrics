80cb1b52b8111d22e67388b59f6acdebc4c30e45
Alec Wydra <alec.wydra2@gmail.com>
2024-06-11 15:25:35 -0400

    Fixed Formatting Error

--- CosineAnalysis.R
+++ CosineAnalysis.R
@@ -1,4 +1,4 @@
-CleanedCosine = subset(FixedGuidelinesCosine, Label != 0)
+CleanedCosine = subset(hw2_garden, Label != 0)
 CleanedCosine = subset(CleanedCosine, global_code_volume != 238)
 
 model = lm(Label ~ cos.similarity, data = CleanedCosine)
