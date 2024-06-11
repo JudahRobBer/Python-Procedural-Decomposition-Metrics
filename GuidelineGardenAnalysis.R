df6c8daa201c9bda0c6cc8e543c489c40df4c710
Alec Wydra <alec.wydra2@gmail.com>
2024-06-11 15:22:35 -0400

    Moved R analysis of new guidlines

---
+++ GuidelineGardenAnalysis.R
@@ -0,0 +1,27 @@
+NotAI = subset(`hw2_garden.(1)`, Label != 0)
+NotAI = subset(NotAI, global_code_volume != 238)
+
+Decent = subset(NotAI, Label > 2)
+
+Good = subset(NotAI, Label > 3)
+
+Great = subset(NotAI, Label > 4)
+
+model = lm(Label ~ largest_information_function + global_code_volume + multiple_output_functions + reused_nodes,data = NotAI)
+summary(model)
+
+model = lm(Label ~ largest_information_function + global_code_volume + multiple_output_functions + reused_nodes, data = Decent)
+model = lm(Label ~ largest_information_function + global_code_volume + multiple_output_functions + reused_nodes, data = Good)
+model = lm(Label ~ largest_information_function + global_code_volume + multiple_output_functions + reused_nodes, data = Great)
+
+
+hist(NotAI$largest_information_function)
+hist(NotAI$multiple_output_functions)
+hist(NotAI$global_code_volume)
+hist(NotAI$reused_nodes)
+
+NotAI$predicted = predict.lm(model,NotAI)
+NotAI$predictdifference = NotAI$predicted - NotAI$Label
+
+boxplot(largest_information_function ~ Label, data = NotAI)
+boxplot(global_code_volume ~ Label, data = NotAI)
